7a1ab74c524b474df6f6229eb2275c36a3734ab6
Your Name <you@example.com>
2023-03-26 11:46:02 +0000

    initial commit

--- R/read_sql_file.R
+++ R/read_sql_file.R
@@ -1,24 +1,24 @@
 #' Les SQL-fil
 #'
-#' Leser innholdet fra en SQL-fil. Filen må være en .sql fil.
+#' Leser innholdet fra en SQL-fil. Filen maa vaere en .sql fil.
 #'
 #' @section Parametrisering:
 #'
 #'
-#' SQL-filen kan inneholde parametere. Disse må være kodet som
+#' SQL-filen kan inneholde parametere. Disse ma vaere kodet som
 #' `{params$<params-navn>}`, f.eks `{params$kalkulasjon}`. Disse angis som input
 #' til funksjonen som en navngitt liste, f.eks
 #' `list(kalkulasjon = "'B MND SBM'")`. Parametriseringen bruker `glue` som
-#' motor, som evaluering av et parameter verdien må gi det innskuddet i filen
-#' som filen behøver. I eksempelet over innføres en tekstparameter i filen.
+#' motor, som evaluering av et parameter verdien maa gi det innskuddet i filen
+#' som filen behoever. I eksempelet over innfoeres en tekstparameter i filen.
 #' Dersom vi skulle sende inn en dato kunne vi gjort slik:
 #' `list(kalkulasjonsdato = "TODATE('2023-01-01','YYYY-MM-DD')")`.
 #'
 #'
 #'
 #'
-#' @param filsti `character/path``. Må angi en fil med endelse .sql eller .SQL.
-#' @param params `list`. Parameteren er valgfri, men må inneholde en nangitt
+#' @param filsti `character/path``. Maa angi en fil med endelse .sql eller .SQL.
+#' @param params `list`. Parameteren er valgfri, men maa inneholde en nangitt
 #' liste med parametere som korresponderer med de i sql-filen.
 #' (A list with the parameters in the corresponding sql file.
 #' @importFrom glue glue
@@ -36,11 +36,11 @@ read_sql_file <- function(filsti,params=NULL){
   }
 
   if (!is.character(filsti))
-    stop("filsti må være tekststregnge")
+    stop("filsti maa vaere tekststregnge")
   if (!file.exists(filsti))
     stop("Finner ikke filen")
   if (tolower(substr(filsti, (nchar(filsti) - 3), nchar(filsti))) != ".sql")
-    stop("Filen må ende med .sql")
+    stop("Filen maa ende med .sql")
   sql_query <-
     filsti %>%
     file("r") %>%
